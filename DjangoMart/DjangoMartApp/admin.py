e386b5853f78b6e8de0d49f77388e866651b8701
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-22 17:02:01 +0300

    register models

--- DjangoMart/DjangoMartApp/admin.py
+++ DjangoMart/DjangoMartApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Category, Product, Brand
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Brand)
