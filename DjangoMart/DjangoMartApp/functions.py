1c6eed7a6474fd8f2186728f293b669820db0198
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-22 17:02:33 +0300

    define a function for cleaner rendering for the app

--- DjangoMart/DjangoMartApp/functions.py
+++ DjangoMart/DjangoMartApp/functions.py
@@ -1,4 +1,6 @@
 from django.db.models import F
+from django.shortcuts import render
+
 
 def add_product_to_cart(cart, product, qty=1):
     cart_item, created = CartItem.objects.get_or_create(
@@ -11,3 +13,10 @@ def add_product_to_cart(cart, product, qty=1):
         cart_item.save()
         cart_item.refresh_from_db()
     return cart_item
+
+
+def render_django_mart_app(request, template_name, context=None):
+    if context is None:
+        context = {}
+    full_template_name = f'DjangoMartApp/{template_name}.html'
+    return render(request, full_template_name, context)
