d1829e4765736ddbb00f070197deecccae8e2cda
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-23 17:13:01 +0300

    register cart and product views

--- DjangoMart/DjangoMartApp/urls.py
+++ DjangoMart/DjangoMartApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login', views.login_view, name='login_view'),
     path('register', views.register, name='register'),
     path('logout', views.logout_view, name='logout_view'),
-    path('category/<str:category_slug>', views.category_view, name='category')
+    path('category/<str:category_slug>', views.category_view, name='category'),
+    path('cart', views.cart_view, name='cart'),
+    path('product/<str:product_id>', views.product_view, name='product')
 ]
