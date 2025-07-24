463fd3d6a68ee5f11356c2b3b78da6e7473f16a0
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-24 16:16:39 +0300

    Track creations and updates of brands

--- DjangoMart/DjangoMartApp/models.py
+++ DjangoMart/DjangoMartApp/models.py
@@ -46,7 +46,9 @@ class Category(models.Model):
 class Brand(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=2000)
-
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return f'{self.title}'
 
