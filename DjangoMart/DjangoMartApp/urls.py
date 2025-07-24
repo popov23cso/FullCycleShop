2679530220d64c53d164d0fd7f94851207aeadb2
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-24 16:16:51 +0300

    Define adding and removing API urls

--- DjangoMart/DjangoMartApp/urls.py
+++ DjangoMart/DjangoMartApp/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', views.homepage_view, name='homepage'),
     path('login', views.login_view, name='login_view'),
     path('register', views.register, name='register'),
     path('logout', views.logout_view, name='logout_view'),
     path('category/<str:category_slug>', views.category_view, name='category'),
     path('cart', views.cart_view, name='cart'),
-    path('product/<str:product_id>', views.product_view, name='product')
+    path('product/<int:product_id>', views.product_view, name='product'),
+    path('add_to_cart', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart', views.remove_from_cart, name='remove_from_cart')
 ]
