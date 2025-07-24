8f2ec00f9d0ae36e77e84a7b933ec3409aec0644
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-24 16:15:44 +0300

    refactor addition to cart function

--- DjangoMart/DjangoMartApp/functions.py
+++ DjangoMart/DjangoMartApp/functions.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from .models import CartItem, ShoppingCart, Purchase, PurchaseItem, Product
 
 
-def add_product_to_cart(cart, product, quantity):
+def add_product_to_cart(user, product, quantity):
+    cart, created = ShoppingCart.objects.get_or_create(user_id=user)
     cart_item, created = CartItem.objects.get_or_create(
         cart=cart,
         product=product,
@@ -12,11 +13,10 @@ def add_product_to_cart(cart, product, quantity):
     if not created:
         cart_item.quantity = F('quantity') + quantity
         cart_item.save()
-        cart_item.refresh_from_db()
-    return cart_item
-
-def product_has_enough_stock(product_id, quantity):
-    product = Product.objects.get(id=product_id)
+    cart.total_items_count += quantity
+    cart.save()
+    
+def product_has_enough_stock(product, quantity):
     if quantity <= product.stock:
         return True 
     return False
