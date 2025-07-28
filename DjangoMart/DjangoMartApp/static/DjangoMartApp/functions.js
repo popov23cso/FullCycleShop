ffb08062aa7bb5598c2537dae8f6c8dbb20ab4e5
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-07-28 17:42:12 +0300

    use get element by id, define function for manipulating int values

--- DjangoMart/DjangoMartApp/static/DjangoMartApp/functions.js
+++ DjangoMart/DjangoMartApp/static/DjangoMartApp/functions.js
@@ -1,8 +1,8 @@
 export function getCSRFToken() {
   return document.cookie
-    .split('; ')
-    .find(row => row.startsWith('csrftoken='))
-    ?.split('=')[1];
+  .split('; ')
+  .find(row => row.startsWith('csrftoken='))
+  ?.split('=')[1];
 }
 
 
@@ -13,16 +13,22 @@ export const toast_background = {
 
 
 export function display_toast(message, body, background) {
-    const toast_element = document.querySelector('#liveToast');
-    const toast_message = document.querySelector('#toastMessage');
-    const toast_body = document.querySelector('#toastBody');
+  const toast_element = document.getElementById('liveToast');
+  const toast_message = document.getElementById('toastMessage');
+  const toast_body = document.getElementById('toastBody');
 
-    toast_element.classList.remove(toast_background.SUCCESS, toast_background.ERROR);
-    toast_element.classList.add(background);
+  toast_element.classList.remove(toast_background.SUCCESS, toast_background.ERROR);
+  toast_element.classList.add(background);
 
-    toast_message.innerText = message;
-    toast_body.innerText = body;
+  toast_message.innerText = message;
+  toast_body.innerText = body;
 
-    const toast = new bootstrap.Toast(toast_element);
-    toast.show();
+  const toast = new bootstrap.Toast(toast_element);
+  toast.show();
+}
+
+
+export function adjust_int_value_by_id(change_ammount, element_id) {
+  const element = document.getElementById(element_id);
+  element.innerText = parseInt(element.innerText, 10) + change_ammount;
 }
--- DjangoMart/DjangoMartApp/static/DjangoMartApp/product.js
+++ DjangoMart/DjangoMartApp/static/DjangoMartApp/product.js
@@ -1,38 +1,39 @@
-import { getCSRFToken, toast_background, display_toast } from './functions.js';
+import { getCSRFToken, toast_background, display_toast, adjust_int_value_by_id } from './functions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector('#addToCartBtn').addEventListener('click', add_to_cart);
+  document.querySelector('#addToCartBtn').addEventListener('click', add_to_cart);
 })
 
 
 async function add_to_cart() {
-    const product_id = document.querySelector('#productId').value;
-    const quantity = document.querySelector('#quantity').value;
+  const product_id = document.getElementById('productId').value;
+  const quantity = parseInt(document.getElementById('quantity').value, 10);
 
-    try {
-    const response = await fetch('/add_to_cart', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': getCSRFToken(),
-      },
-      body: JSON.stringify({
-        product_id: product_id,
-        quantity: quantity
-      })
-    });
+  try {
+  const response = await fetch('/add_to_cart', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRFToken': getCSRFToken(),
+    },
+    body: JSON.stringify({
+      product_id: product_id,
+      quantity: quantity
+    })
+  });
 
-    const data = await response.json();
-  
-    if (data.error) {
-      display_toast('Error!', data.error, toast_background.ERROR);
-    } else {
-      display_toast('Success!', data.message, toast_background.SUCCESS);
-    }
+  const data = await response.json();
+
+  if (data.error) {
+    display_toast('Error!', data.error, toast_background.ERROR);
+  } else {
+    display_toast('Success!', data.message, toast_background.SUCCESS);
+    adjust_int_value_by_id(quantity, 'cartItemsCount');
+  }
   } catch (err) {
     console.error('Fetch error:', err);
     alert('Something went wrong.');
   }
 }
+
 
