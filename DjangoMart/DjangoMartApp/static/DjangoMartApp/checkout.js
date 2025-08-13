c25d657aae2ba51c0f00d26d6cbfcea3cc557324
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-08-13 11:59:49 +0300

    Replace getelementbyid with query selectors for more modern approach. add functionality to dynamically add and remove addresses in checkout

--- DjangoMart/DjangoMartApp/static/DjangoMartApp/checkout.js
+++ DjangoMart/DjangoMartApp/static/DjangoMartApp/checkout.js
@@ -1,27 +1,31 @@
-import { getCSRFToken, toast_background, display_toast} from './functions.js';
+import { getCSRFToken, toast_background, display_toast, hide_element, show_element} from './functions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('saveAddressBtn').addEventListener('click', save_address);
+    document.querySelector('#saveAddressBtn').addEventListener('click', save_address);
 
-  document.querySelectorAll('.deleteAddressBtn').forEach(button => {
-    button.addEventListener('click', e => {
-        const btn = e.currentTarget;
+    const container = document.querySelector('#addressesContainer');
+    container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('deleteAddressBtn')) {
+        const btn = event.target;
         const parent = btn.closest('.card');
         const { city, street, streetnum, phonenum } = btn.dataset;
-            remove_address(parent, city, street, streetnum, phonenum);
-        });
+        remove_address(parent, city, street, streetnum, phonenum);
+    }
     });
+
 })
 
 
 
 async function save_address() {
-    const city = document.getElementById('dropdownCity').value;
-    const street = document.getElementById('dropdownStreet').value;
-    const street_number = document.getElementById('dropdownStreetNumber').value;
-    const phone_number = document.getElementById('dropdownPhoneNumber').value;
-    const address_dropdown = document.getElementById('addressDropdown');
-    const address_menu = document.getElementById('addressMenu');
+    const city = document.querySelector('#dropdownCity').value;
+    const street = document.querySelector('#dropdownStreet').value;
+    const street_number = document.querySelector('#dropdownStreetNumber').value;
+    const phone_number = document.querySelector('#dropdownPhoneNumber').value;
+    const address_dropdown = document.querySelector('#addressDropdown');
+    const address_menu = document.querySelector('#addressMenu');
+    const address_container = document.querySelector('#addressesContainer');
+    let address_count = parseInt(address_container.dataset.addresscount);
 
     try {
         const response = await fetch ('/add_address', {
@@ -43,6 +47,12 @@ async function save_address() {
             } else {
                 display_toast('Success!', 'Added successfully', toast_background.SUCCESS);
                 display_address(city, street, street_number, phone_number);
+                address_count += 1;
+                if (address_count >= 3) {
+                    let address_dropdown_selector = '#addressDropdown';
+                    hide_element(address_dropdown_selector);
+                }
+                address_container.dataset.addresscount = address_count;
             }
     } catch (err) {
         console.error('Fetch error:', err);
@@ -53,6 +63,8 @@ async function save_address() {
 }
 
 async function remove_address(parent_element, city, street, street_number, phone_number) {
+    const address_container = document.querySelector('#addressesContainer');
+    let address_count = parseInt(address_container.dataset.addresscount);
     try {
         const response = await fetch ('/remove_address', {
             method: 'PUT',
@@ -73,6 +85,11 @@ async function remove_address(parent_element, city, street, street_number, phone
             } else {
                 display_toast('Success!', 'Removed successfully', toast_background.SUCCESS);
                 parent_element.remove();
+                address_count -= 1;
+                let address_dropdown_selector = '#addressDropdown';
+                show_element(address_dropdown_selector);
+                address_container.dataset.addresscount = address_count;
+
             }
     } catch (err) {
         console.error('Fetch error:', err);
@@ -81,16 +98,22 @@ async function remove_address(parent_element, city, street, street_number, phone
 }
 
 function display_address(city, street, street_number, phone_number) {
-    const template = document.getElementById('addressTemplate');
+    const template = document.querySelector('#addressTemplate');
     const clone = template.content.cloneNode(true);
-    const city_el = clone.getElementById('city');
-    const street_el = clone.getElementById('street');
-    const street_num_el = clone.getElementById('streetNum');
-    const phone_num_el = clone.getElementById('phoneNum');
+    const city_el = clone.querySelector('#city');
+    const street_el = clone.querySelector('#street');
+    const street_num_el = clone.querySelector('#streetNum');
+    const phone_num_el = clone.querySelector('#phoneNum');
+    const delete_btn = clone.querySelector('.deleteAddressBtn');
+
     city_el.textContent = city;
     street_el.textContent = street;
     street_num_el.textContent = street_number;
     phone_num_el.textContent = phone_number;
+    delete_btn.dataset.city = city;
+    delete_btn.dataset.street = street;
+    delete_btn.dataset.streetnum = street_number;
+    delete_btn.dataset.phonenum = phone_number;
 
-    document.getElementById('addressesContainer').appendChild(clone);
+    document.querySelector('#addressesContainer').appendChild(clone);
 }
--- DjangoMart/DjangoMartApp/static/DjangoMartApp/functions.js
+++ DjangoMart/DjangoMartApp/static/DjangoMartApp/functions.js
@@ -13,9 +13,9 @@ export const toast_background = {
 
 
 export function display_toast(message, body, background) {
-  const toast_element = document.getElementById('liveToast');
-  const toast_message = document.getElementById('toastMessage');
-  const toast_body = document.getElementById('toastBody');
+  const toast_element = document.querySelector('#liveToast');
+  const toast_message = document.querySelector('#toastMessage');
+  const toast_body = document.querySelector('#toastBody');
 
   toast_element.classList.remove(toast_background.SUCCESS, toast_background.ERROR);
   toast_element.classList.add(background);
@@ -29,6 +29,16 @@ export function display_toast(message, body, background) {
 
 
 export function adjust_int_value_by_id(change_ammount, element_id) {
-  const element = document.getElementById(element_id);
+  const element = document.querySelector('#' + element_id);
   element.innerText = parseInt(element.innerText, 10) + change_ammount;
+}
+
+export function hide_element(selector) {
+  const element = document.querySelector(selector);
+  element.style.display = 'none';
+}
+
+export function show_element(selector) {
+  const element = document.querySelector(selector);
+  element.style.display = 'block';
 }
