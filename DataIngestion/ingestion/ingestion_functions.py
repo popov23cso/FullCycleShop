356132a02a03b602fb3694db4520032dfc4ee204
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-08-19 17:45:12 +0300

    Define base Django Mart data ingestion functions

--- DataIngestion/ingestion/ingestion_functions.py
+++ DataIngestion/ingestion/ingestion_functions.py
@@ -1,1 +1,84 @@
+import requests
+import duckdb
+import datetime
+from pathlib import Path 
+import pandas as pd
+from api_secrets import DJANGOMART_PASSWORD, DJANGOMART_USERNAME
 
+def get_djangomart_auth_tokens(username, password):
+    token_url = 'http://127.0.0.1:8000/api/token/'
+    request_body = {
+        'username':username,
+        'password':password
+    }
+    try:
+        response = requests.post(token_url, json=request_body)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"Error while requesting token server: {e}")
+    
+    data = response.json()
+    if 'access' not in data or 'refresh' not in data:
+        raise ValueError(f"Unexpected response format while retrieving auth tokens: {data}")
+    
+    return data['refresh'], data['access']
+
+
+def refresh_access_token(refresh_token):
+    token_url = 'http://127.0.0.1:8000/api/token/refresh/'
+    request_body = {
+        'refresh':refresh_token,
+    }
+    try:
+        response = requests.post(token_url, json=request_body)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"Error while requesting access token from token server: {e}")
+    
+    data = response.json()
+    if 'access' not in data:
+        raise ValueError(f"Unexpected response format while retrieving access token: {data}")
+    
+    return data['access']
+
+def get_djangomart_data(access_token, endpoint, data_key, updated_after):
+    base_url = 'http://127.0.0.1:8000/'
+    full_url = base_url + endpoint
+
+    headers = {
+    "Authorization": f"Bearer {access_token}"
+    }
+
+    params = {
+        'updated_after': updated_after
+    }
+
+    try:
+        response = requests.get(full_url, headers=headers, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f"Error while requesting {endpoint} data: {e}")
+    
+    response_data = response.json()
+    if response_data['success']:
+        current_datetime = datetime.datetime.now()
+        current_datetime = current_datetime.strftime('%Y%m%d%H%M%S')
+
+
+        file_name = endpoint + '_' + current_datetime + '.parquet'
+
+        current_dir = Path.cwd()
+        file_path = current_dir.parents[1] / "DataLake" / "DjangoMart" / file_name
+
+        object_data = response_data[data_key]
+
+        # duckdb cannot read directly in memory python objects so an intermediate step is needed
+        df = pd.DataFrame(object_data)
+
+        with duckdb.connect() as con:
+            con.register("object_data", df)
+            con.execute(
+                f"COPY (SELECT * FROM object_data) TO '{file_path}' (FORMAT PARQUET)"
+            )
+
+        return file_name
