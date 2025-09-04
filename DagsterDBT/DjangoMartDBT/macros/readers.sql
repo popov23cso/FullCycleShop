63c2a2d022074c30f84440c394f3f135bc29e245
popov23cso <popov23cso@gmail.com>
2025-09-04 17:51:19 +0300

    Restructure layers, define historic macros and new readers

--- DagsterDBT/DjangoMartDBT/macros/readers.sql
+++ DagsterDBT/DjangoMartDBT/macros/readers.sql
@@ -1,3 +1,19 @@
 {% macro read_data_lake_folder(folder_path) %}
-    SELECT * FROM '{{ var("DATA_LAKE_PATH") ~ folder_path ~ "/*.parquet"}}'
+    SELECT 
+        *,
+        filename AS DWH_FILE_PATH,
+        substring
+        (    
+            filename
+            from length(filename) - 7 - 14
+            for 13
+        ) AS DWH_BATCH_DATETIME
+    FROM read_parquet('{{ var("DATA_LAKE_PATH") ~ folder_path ~ "/*.parquet"}}')
+{% endmacro %}
+
+
+{% macro list_raw_metadata() %}
+    DWH_BATCH_DATETIME AS DWH_BATCH_DATETIME_STR,
+    strptime(DWH_BATCH_DATETIME, '%Y%m%d%H%M%S')::TIMESTAMP AS DWH_BATCH_DATETIME,
+    strptime(DWH_BATCH_DATETIME, '%Y%m%d%H%M%S')::DATE AS DWH_BATCH_DATE,
 {% endmacro %}
