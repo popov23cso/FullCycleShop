060b70f9c4b3de2a9dab9d4258a7601228d05811
popov23cso <popov23cso@gmail.com>
2025-09-09 11:20:07 +0300

    DRY-up validation logic

--- DjangoMart/DjangoMartApp/views/utility.py
+++ DjangoMart/DjangoMartApp/views/utility.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from ..models import CartItem, ShoppingCart
 from datetime import datetime
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
 
 
 class ApiPagination(PageNumberPagination):
@@ -42,3 +45,30 @@ def parse_date(date):
     except (TypeError, ValueError):
         return None
 
+def validate_api_date_parameters(created_after, updated_after):
+    created_after = parse_date(created_after)
+    updated_after = parse_date(updated_after)
+
+    validation_error = None
+    if updated_after is not None and created_after is not None:
+        validation_error =  'Created_after and updated_after cannot be passed at the same time'
+    elif updated_after is None and created_after is None:
+        validation_error =  'Supplying updated_after or created_after parameter is required. Passed dates need to be in valid ISO 8601 format'
+
+    if validation_error is not None:
+        validation_response = Response(
+            {
+                'success':False,
+                'error': validation_error
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+        
+        return False, validation_response
+    
+    filter_date = updated_after if updated_after is not None else created_after
+
+    # when no timezone is provided by the user default to the server default timezone 
+    if timezone.is_naive(filter_date):
+        filter_date = timezone.make_aware(filter_date)
+        
+    return True, filter_date
