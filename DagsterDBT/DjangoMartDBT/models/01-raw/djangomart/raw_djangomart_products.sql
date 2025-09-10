a63f21d8cd47b8856bbc027cbb505eeabf2591ae
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-09-10 17:34:23 +0300

    small changes

--- DagsterDBT/DjangoMartDBT/models/01-raw/djangomart/raw_djangomart_products.sql
+++ DagsterDBT/DjangoMartDBT/models/01-raw/djangomart/raw_djangomart_products.sql
@@ -3,4 +3,4 @@ WITH dummy_dagster_relation AS
     SELECT * FROM {{ source('djangomart_products', 'dummy') }}
 )
 
-{{ read_data_lake_folder('DJANGOMART/PRODUCTS')}}
+{{ read_data_lake_folder('DJANGOMART/GET_PRODUCTS')}}
