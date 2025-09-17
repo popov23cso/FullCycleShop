22047bb098f0b052e4d156d0eebcf90cebd95cf7
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-09-17 11:18:54 +0300

    Make historic layer incremental with consistent delta column

--- DagsterDBT/DjangoMartDBT/macros/historize.sql
+++ DagsterDBT/DjangoMartDBT/macros/historize.sql
@@ -12,14 +12,19 @@
 {% endmacro%}
 
 
-{% macro historize_relation(relation_name, id_column, delta_column) %}
+{% macro historize_relation(relation_name, id_column) %}
 
     WITH relation_ranked AS 
     (
         SELECT
             r.*,
-            ROW_NUMBER() OVER (PARTITION BY {{id_column}} ORDER BY {{delta_column}} DESC) as row_rank
+            ROW_NUMBER() OVER (PARTITION BY {{id_column}} ORDER BY DWH_BATCH_DATETIME DESC) as row_rank
         FROM {{ ref(relation_name) }} r
+
+        {% IF is_incremental() %}
+        -- only pull new/changed rows
+            WHERE DWH_BATCH_DATETIME > COALESCE((SELECT max(DWH_BATCH_DATETIME) FROM {{ this }}), {{var('BEGINNING_OF_TIME')}})
+        {% endif %}
     )
     SELECT 
         CONCAT({{id_column}}, '_', DWH_BATCH_DATETIME_STR) AS DWH_SK,
