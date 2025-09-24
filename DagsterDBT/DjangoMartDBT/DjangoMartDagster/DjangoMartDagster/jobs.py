3d9a12e55b551e8acb7b8e673c48c2c76be6a4d0
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-09-24 17:08:21 +0300

    Define tags for each dbt layer and define a job based on asset selection for djangomart data source

--- DagsterDBT/DjangoMartDBT/DjangoMartDagster/DjangoMartDagster/jobs.py
+++ DagsterDBT/DjangoMartDBT/DjangoMartDagster/DjangoMartDagster/jobs.py
@@ -1,9 +1,9 @@
-from dagster import define_asset_job, AssetKey
+from dagster import define_asset_job, AssetSelection
 
 djnagomart_job = define_asset_job(
                                     name='daily_djangomart_job',
 
                                     # drop selection if i want to run all assets with this job
-                                    selection=['get_purchases', 'raw/raw_djangomart_purchases'] 
+                                    selection=(AssetSelection.groups('djangomart_api_objects') | AssetSelection.tag('djangomart-dbt', ''))
                                  )
 
