665d3c59d4f5759d10099a1061d8774ca30aa7b8
popov23cso <popov23cso@gmail.com>
2025-09-26 11:36:39 +0300

    Implement a base command class

--- DjangoMart/DjangoMartApp/management/commands/seed_categories.py
+++ DjangoMart/DjangoMartApp/management/commands/seed_categories.py
@@ -1,16 +1,6 @@
-from django.core.management.base import BaseCommand
+from base_command_class import BaseSeedCommand
 from ...factories import CategoryFactory
 
-class Command(BaseCommand):
-    help = "Seed the app database with fake categories"
-
-    def add_arguments(self, parser):
-        parser.add_argument('--count',
-                            type=int, 
-                            default=5,
-                            choices=(1, 501))
-
-    def handle(self, *args, **options):
-        count = options['count']
-        CategoryFactory.create_batch(count)
-        self.stdout.write(self.style.SUCCESS(f"Created {count} users"))
+class Command(BaseSeedCommand):
+    help = "Seed Category objects"
+    factory_class = CategoryFactory
--- DjangoMart/DjangoMartApp/management/commands/seed_users.py
+++ DjangoMart/DjangoMartApp/management/commands/seed_users.py
@@ -1,16 +1,6 @@
-from django.core.management.base import BaseCommand
+from base_command_class import BaseSeedCommand
 from ...factories import UserFactory
 
-class Command(BaseCommand):
-    help = "Seed the app database with fake users"
-
-    def add_arguments(self, parser):
-        parser.add_argument('--count',
-                            type=int, 
-                            default=5,
-                            choices=(1, 501))
-
-    def handle(self, *args, **options):
-        count = options['count']
-        UserFactory.create_batch(count)
-        self.stdout.write(self.style.SUCCESS(f"Created {count} users"))
+class Command(BaseSeedCommand):
+    help = "Seed User objects"
+    factory_class = UserFactory
