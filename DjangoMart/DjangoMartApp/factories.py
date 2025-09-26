c8389a3522d3a7e4f2ecf42fa5791b2f74cdf51c
popov23cso <popov23cso@gmail.com>
2025-09-26 11:36:58 +0300

    Implement base factory class and define model factories

--- DjangoMart/DjangoMartApp/factories.py
+++ DjangoMart/DjangoMartApp/factories.py
@@ -2,86 +2,95 @@ import factory
 from faker import Faker
 from django.contrib.auth.hashers import make_password
 from .models import (User, Brand, Category,
-                     Product)
+                     Product, Purchase, PurchaseItem,
+                     Review)
 from datetime import datetime
-import random
+from django.utils.text import slugify
 
 fake = Faker()
 
-class UserFactory(factory.django.DjangoModelFactory):
+class BaseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        abstract = True
+
+    # define the common columns that are present accross all models
+    created_date = factory.LazyFunction(
+        lambda: fake.date_time_between(
+            start_date=datetime(2023, 1, 1), 
+            end_date=datetime(2025, 12, 31)
+        )
+    )
+    updated_date = factory.SelfAttribute('created_date')
+    is_auto_generated = True
+
+class UserFactory(BaseFactory):
     class Meta:
         model = User
 
-    username = factory.LazyAttribute(lambda _: fake.user_name())
-    email = factory.LazyAttribute(lambda _: fake.email())
-    first_name = factory.LazyAttribute(lambda _: fake.first_name())
-    last_name = factory.LazyAttribute(lambda _: fake.last_name())
-    available_tokens = factory.LazyAttribute(lambda _: fake.random_int(min=0, max=10000))
-    password = factory.LazyFunction(lambda: make_password("defaultpassword123"))
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    available_tokens = factory.Faker('pyint', min_value=0, max_value=20000)
+    password = factory.LazyFunction(lambda: make_password('defaultpassword123'))
 
-    # generate random datetimes that are the same for created and updated date (between 2023 and 2025)
-    created_date = factory.LazyFunction(
-        lambda: fake.date_time_between(start_date=datetime(2023, 1, 1), end_date=datetime(2025, 12, 31))
-    )
-    updated_date = factory.SelfAttribute("created_date")
 
-    is_auto_generated = True
-
-class BrandFactory(factory.django.DjangoModelFactory):
+class BrandFactory(BaseFactory):
     class Meta:
         model = Brand
 
-    title = factory.LazyAttribute(lambda _: fake.word().title())
-    description = factory.LazyAttribute(lambda _: fake.word())
+    title = factory.Faker('catch_phrase')
+    description = factory.Faker('word')
 
-    # generate random datetimes that are the same for created and updated date (between 2023 and 2025)
-    created_date = factory.LazyFunction(
-        lambda: fake.date_time_between(start_date=datetime(2023, 1, 1), end_date=datetime(2025, 12, 31))
-    )
-    updated_date = factory.SelfAttribute("created_date")
-    
-    is_auto_generated = True
-
-class CategoryFactory(factory.django.DjangoModelFactory):
+class CategoryFactory(BaseFactory):
     class Meta:
         model = Category
 
-    title = factory.LazyAttribute(lambda _: fake.word().title())
-    slug = factory.LazyAttribute(lambda obj: obj.title.lower())
-    description = factory.LazyAttribute(lambda _: fake.word())
+    title = factory.Faker('catch_phrase')
+
+    # turn title with spaces into valid slugs - Car Parts => car-parts
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.title))
+    description = factory.Faker('word')
     is_main_category = True 
     is_active = True
 
-    # generate random datetimes that are the same for created and updated date (between 2023 and 2025)
-    created_date = factory.LazyFunction(
-        lambda: fake.date_time_between(start_date=datetime(2023, 1, 1), end_date=datetime(2025, 12, 31))
-    )
-    updated_date = factory.SelfAttribute("created_date")
-    
-    is_auto_generated = True
-
-class ProductFactory(factory.django.DjangoModelFactory):
+class ProductFactory(BaseFactory):
     class Meta:
         model = Product
 
-    title = factory.LazyAttribute(lambda _: fake.word().title())
-    description = factory.LazyAttribute(lambda _: fake.word())
-    price = factory.LazyAttribute(lambda _: fake.random_int(min=250, max=4500))
-    brand = factory.LazyFunction(lambda: random.choice(Brand.objects.all()))
-    stock = factory.LazyAttribute(lambda _: fake.random_int(min=0, max=250))
-    rating = factory.LazyAttribute(lambda _: fake.random_float(min=0.0, max=5.0))
-    ratings_count = factory.LazyAttribute(lambda _: fake.random_int(min=0, max=900))
-    sales_count = factory.LazyAttribute(lambda _: fake.random_int(min=0, max=1000))
-    category = factory.LazyFunction(lambda: random.choice(Category.objects.all()))
+    title = factory.Faker('catch_phrase')
+    description = factory.Faker('word')
+    price = factory.Faker('pyint', min_value=250, max_value=4500)
+    brand = factory.Iterator(Brand.objects.all())
+    stock = factory.Faker('pyint', min_value=0, max_value=250)
+    rating = factory.Faker('pyfloat', min_value=0.0, max_value=5.0, right_digits=2)
+    ratings_count = factory.Faker('pyint', min_value=0, max_value=900)
+    sales_count = factory.Faker('pyint', min_value=0, max_value=1000)
+    category = factory.Iterator(Category.objects.all())
 
-    # generate random datetimes that are the same for created and updated date (between 2023 and 2025)
-    created_date = factory.LazyFunction(
-        lambda: fake.date_time_between(start_date=datetime(2023, 1, 1), end_date=datetime(2025, 12, 31))
-    )
-    updated_date = factory.SelfAttribute("created_date")
-    
-    is_auto_generated = True
+class PurchaseFactory(BaseFactory):
+    class Meta:
+        model = Purchase
 
-    
-    
+    user = factory.Iterator(User.objects.all())
+    total_price = factory.Faker('pyint', min_value=250, max_value=9000)
+
+class PurchaseItemFactory(BaseFactory):
+    class meta:
+        model = PurchaseItem
+
+    purchase = factory.Iterator(Purchase.objects.all())
+    product = factory.Iterator(Product.objects.all())
+    product_name = factory.LazyAttribute(lambda obj: obj.product.title)
+    quantity = factory.Faker('pyint', min_value=1, max_value=5)
+    price_at_purchase = factory.LazyAttribute(lambda obj: obj.quantity * obj.product.price)
+
+class ReviewFactory(BaseFactory):
+    class Meta:
+        model = Review
+    user = factory.Iterator(User.objects.all())
+    purchase_item = factory.SubFactory(PurchaseItemFactory)
+    product = factory.LazyAttribute(lambda obj: obj.purchase_item.product)
+    rating = factory.Faker('pyint', min_value=1, max_value=5)
+    comment = factory.Faker('sentence')
     
