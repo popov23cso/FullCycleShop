562361c4cd286d531f7820c5999d0f0e465ecd72
popov23cso <popov23cso@gmail.com>
2025-10-06 12:28:02 +0300

    Correct dimensioning logic, use appropriate delta column

--- DjangoMartDBT/models/04-dimensioned/scd2/01-integrated/djangomart/integrated_djangomart_products.sql
+++ DjangoMartDBT/models/04-dimensioned/scd2/01-integrated/djangomart/integrated_djangomart_products.sql
@@ -24,24 +24,49 @@ integrated_category_dates AS
     SELECT 
         p.* EXCLUDE (DWH_DELTA_DATE, next_delta, DWH_SK),
         HASH(p.DWH_SK, c.DWH_SK) AS DWH_SK,
-        c.DWH_BATCH_DATETIME AS DWH_DELTA_DATE
+        c.DWH_BATCH_DATETIME AS DWH_DELTA_DATE,
     FROM product_base p
     JOIN category_dates c ON c.ID = p.CATEGORY_ID
     AND c.DWH_BATCH_DATETIME >= p.DWH_BATCH_DATETIME
     AND c.DWH_BATCH_DATETIME < p.next_delta
 ),
-product_history AS (
+product_history AS 
+(
     SELECT 
         *
     FROM {{ ref('historic_djangomart_products') }}
+),
+all_dates_integrated AS 
+(
+    SELECT 
+        {{ dbt_utils.star(
+            from=ref('historic_djangomart_products'),
+            except=['DWH_IS_LATEST']) 
+        }},
+        'product' AS CHANGE_SOURCE
+    FROM product_history
+    UNION ALL 
+    SELECT
+        {{ dbt_utils.star(
+            from=ref('historic_djangomart_products'),
+            except=['DWH_IS_LATEST']) 
+        }},
+        'category' AS CHANGE_SOURCE
+    FROM integrated_category_dates
+),
+dwh_is_latest_recalculated AS 
+(
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY ID ORDER BY DWH_DELTA_DATE DESC) AS row_rank,
+        CASE
+            WHEN row_rank = 1 THEN 1
+            ELSE 0
+        END AS DWH_IS_LATEST
+    FROM all_dates_integrated
 )
 
 SELECT 
-    {{ dbt_utils.star(from=ref('historic_djangomart_products')) }},
-    'product' AS CHANGE_SOURCE
-FROM product_history
-UNION ALL 
-SELECT
-    {{ dbt_utils.star(from=ref('historic_djangomart_products')) }},
-    'category' AS CHANGE_SOURCE
-FROM integrated_category_dates
+    * EXCLUDE(row_rank)
+FROM dwh_is_latest_recalculated
+
--- DjangoMartDBT/models/04-dimensioned/scd2/02-implemented/djangomart/scd2_djangomart_products.sql
+++ DjangoMartDBT/models/04-dimensioned/scd2/02-implemented/djangomart/scd2_djangomart_products.sql
@@ -1,4 +1,5 @@
 {{ generate_scd2(
     relation_name='integrated_djangomart_products',
-    id_column='ID'
+    id_column='ID',
+    delta_column='DWH_DELTA_DATE'
 ) }}
