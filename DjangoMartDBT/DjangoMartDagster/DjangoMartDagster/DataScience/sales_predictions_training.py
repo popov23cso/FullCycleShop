03da194ce4eecedbb4d92d8e96b1b022d41e3391
popov23cso <97740486+popov23cso@users.noreply.github.com>
2025-10-13 12:00:09 +0300

    Move functionality in a separate module

--- DjangoMartDBT/DjangoMartDagster/DjangoMartDagster/DataScience/sales_predictions_training.py
+++ DjangoMartDBT/DjangoMartDagster/DjangoMartDagster/DataScience/sales_predictions_training.py
@@ -1,11 +1,9 @@
 import duckdb
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from pathlib import Path 
 from dagster import op
-from sklearn.preprocessing import StandardScaler
-import numpy as np
+from .utility import split_and_encode_sales_data, scale_sales_data
 
 @op
 def train_sales_sequential_prediction_model(context):
@@ -64,49 +62,3 @@ def train_sales_sequential_prediction_model(context):
     model.save(model_path)
 
     context.log.info(f'Model Saved to: {model_path}')
-
-def split_and_encode_sales_data(sales_df):
-    # day of week (0-6) and, circular values, tend to confuse models
-    # encoding them helps model interpret them better
-    sales_df['DAY_SIN'] = np.sin(2 * np.pi * sales_df['DAY_OF_THE_WEEK'] / 7)
-    sales_df['DAY_COS'] = np.cos(2 * np.pi * sales_df['DAY_OF_THE_WEEK'] / 7)
-
-    sales_df['MONTH_SIN'] = np.sin(2 * np.pi * (sales_df['RECORD_MONTH']-1) / 12)
-    sales_df['MONTH_COS'] = np.cos(2 * np.pi * (sales_df['RECORD_MONTH']-1) / 12)
-
-    # split data into two batches - 80% of all data towards training, 20% towards testing
-    # since data is time based no random split is applied
-    split_size = int(len(sales_df) * 0.8)
-    training_data = sales_df.iloc[:split_size]
-    testing_data = sales_df.iloc[split_size:]
-
-    X_columns = ['TOTAL_TRANSACTIONS_COUNT', 'DAY_SIN', 'DAY_COS',
-                'IS_WEEKEND', 'LAST_DAY_SALES', 'LAST_7_DAYS_SALES',
-                'MONTH_SIN', 'MONTH_COS', 'LAST_14_DAYS_SALES',
-                'LAST_30_DAYS_SALES']
-    Y_columns = ['TOTAL_TOKENS_SPENT']
-
-    training_data_x = training_data[X_columns]
-    training_data_y = training_data[Y_columns]
-
-    testing_data_x = testing_data[X_columns]
-    testing_data_y = testing_data[Y_columns]
-
-    return training_data_x, training_data_y, testing_data_x, testing_data_y
-
-def scale_sales_data(training_data_x, testing_data_x):
-
-    # normalize feature values. mean values of 0 with standart 
-    # deviation of 1. makes data more consistent, predictable and balanced
-    numeric_cols = ['TOTAL_TRANSACTIONS_COUNT', 'LAST_DAY_SALES', 'LAST_7_DAYS_SALES',
-                    'LAST_14_DAYS_SALES', 'LAST_30_DAYS_SALES']
-
-    scaler = StandardScaler()
-
-    training_data_x_scaled = training_data_x
-    training_data_x_scaled[numeric_cols] = scaler.fit_transform(training_data_x[numeric_cols])
-
-    # apply scaling derived from training data to testing data
-    # ensures training and testing data is treated the same
-    testing_data_x_scaled = testing_data_x
-    testing_data_x_scaled[numeric_cols] = scaler.transform(testing_data_x[numeric_cols])
